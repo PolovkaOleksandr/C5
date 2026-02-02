c64724b44abd5858d6a768f3ef0c330117e2751e
PolovkaOleksandr <120940683+PolovkaOleksandr@users.noreply.github.com>
2026-02-02 22:16:06 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+             try
+             {
+                 Console.WriteLine("enter a number");
+                 int a = Convert.ToInt32(Console.ReadLine());
+
+             }
+             catch (FormatException)
+             {
+                Console.WriteLine($"An error occurred: Invalid input. Please enter a valid integer.");
+             }
+        }
+    }
+}
